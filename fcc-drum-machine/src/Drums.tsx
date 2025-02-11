82d2d705e520d14c7ad6e7032af683a52a1ed314
gor-mirzakhanyan2005 <gor.mirzakhanyan0508@gmail.com>
2025-02-11 18:59:06 +0100

    Project Commit 2
    The final commit

--- fcc-drum-machine/src/Drums.tsx
+++ fcc-drum-machine/src/Drums.tsx
@@ -8,97 +8,111 @@ import song7 from "./assets/Kick_n_Hat.mp3";
 import song8 from "./assets/RP4_KICK_1.mp3";
 import song9 from "./assets/Cev_H2.mp3";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from "./Drums.module.css";
 
 const Drums = () => {
 
+    const songsAndKeys = useRef([
+        {
+            trigger: "q",
+            sound: song1,
+            desc: "Heater 1",
+            id: "q"
+        },
+        {
+            trigger: "w",
+            sound: song2,
+            desc: "Heater 2",
+            id: "w"
+        },
+        {
+            trigger: "e",
+            sound: song3,
+            desc: "Heater 3",
+            id: "e"
+        },
+        {
+            trigger: "a",
+            sound: song4,
+            desc: "Heater 4",
+            id: "a"
+        },
+        {
+            trigger: "s",
+            sound: song5,
+            desc: "Clap",
+            id: "s"
+        },
+        {
+            trigger: "d",
+            sound: song6,
+            desc: "Open-HH",
+            id: "d"
+        },
+        {
+            trigger: "z",
+            sound: song7,
+            desc: "Kick-n'-Hat",
+            id: "z"
+        },
+        {
+            trigger: "x",
+            sound: song8,
+            desc: "Kick",
+            id: "x"
+        },
+        {
+            trigger: "c",
+            sound: song9,
+            desc: "Closed-HH",
+            id: "c"
+        }
+    ])
+
     const [active, setActive] = useState<string>("");
     const [volume, setVolume] = useState<number>(0.5);
-
-    const songsAndKeys: {[index: string]: HTMLAudioElement} = {
-        "q": new Audio(song1),
-        "w": new Audio(song2),
-        "e": new Audio(song3),
-        "a": new Audio(song4),
-        "s": new Audio(song5),
-        "d": new Audio(song6),
-        "z": new Audio(song7),
-        "x": new Audio(song8),
-        "c": new Audio(song9)
-    }
-
-    useEffect(()=>{
-        Object.values(songsAndKeys).forEach(song => song.volume = volume)
-    }, [volume])
+    const audioRef = useRef<(HTMLAudioElement)[]>([]);
 
     useEffect(() => {
-        window.addEventListener("keydown", event => {
-            const pushedKey = event.key;
-            console.log(pushedKey);
-            const corrSong: HTMLAudioElement = songsAndKeys[pushedKey];
+        const handleKeyDown = (event: KeyboardEvent) => {
+            const downKey = event.key;
+            console.log(downKey);
 
-            setActive(pushedKey);
-            corrSong.currentTime = 0;
-            corrSong.play();
-        })
-
-        window.addEventListener("keyup", () => {
-            setActive("");
-        })
-
-        return () => {
-            window.removeEventListener("keydown", event => {
-                const pushedKey = event.key;
-                console.log(pushedKey);
-                const corrSong: HTMLAudioElement = songsAndKeys[pushedKey];
-
-                setActive(pushedKey);
-                corrSong.volume = volume;
-                corrSong.currentTime = 0;
-                corrSong.play();
-            })
-
-            window.removeEventListener("keyup", () => {
-                setActive("");
-            })
+            for(let i = 0; i < songsAndKeys.current.length; i++){
+                const soundIndex = audioRef.current.findIndex(song => song.id === songsAndKeys.current[i].id)
+                if(audioRef.current[soundIndex].id === downKey){
+                    audioRef.current[soundIndex].volume = volume;
+                    audioRef.current[soundIndex].play();
+                    setActive(songsAndKeys.current[i].id)
+                }
+            }
         }
-    }, [])
+
+        const handleKeyUp = () => {
+            setActive("");
+        }
+
+        window.addEventListener("keydown", handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
+    })
 
     return (
         <>
             <main id={styles['drumsMain']}>
                 <div id={styles['drum-machine']}>
                     <div id={styles['display']}>
-                        <div id={styles[`drumKey1${active === "q" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            Q
-                        </div>
-                        <div id={styles[`drumKey2${active === "w" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            W
-                        </div>
-                        <div id={styles[`drumKey3${active === "e" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            E
-                        </div>
-                        <div id={styles[`drumKey4${active === "a" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            A
-                        </div>
-                        <div id={styles[`drumKey5${active === "s" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            S
-                        </div>
-                        <div id={styles[`drumKey6${active === "d" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            D
-                        </div>
-                        <div id={styles[`drumKey7${active === "z" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            Z
-                        </div>
-                        <div id={styles[`drumKey8${active === "x" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            X
-                        </div>
-                        <div id={styles[`drumKey9${active === "c" ? "Active" : ""}`]} className={styles['drum-pad']}>
-                            C
-                        </div>
+                        {songsAndKeys.current.map((drum, index)  => {
+                            return <div onClick={() => {
+                                audioRef.current[index].volume = volume;
+                                audioRef.current[index].play();
+                            }} className={styles['drum-pad']} id={styles[`drumKey${drum.id.toUpperCase()}${active === drum.id ? "Active" : ""}`]}>
+                                {drum.trigger.toUpperCase()}
+                                <audio id={drum.id} ref={(el: HTMLAudioElement) => { audioRef.current[index] = el} } src={drum.sound}></audio>
+                                </div>
+                        })}
                     </div>
-                    <input type="range" min="0" max="1" step="0.01" onChange={(event) => setVolume(Number(event.target.value))} id={styles['volume']} />
+                    <input onChange={(e) => setVolume(Number(e.target.value))} value={volume} type="range" min="0" max="1" step="0.01" id={styles['volume']} />
                 </div>
             </main>
         </>
